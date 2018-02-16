eedbcf830c431c0fee56e5419313b23ad4766ee6
a7medfawzy97 <36432072+a7medfawzy97@users.noreply.github.com>
2018-02-16 10:53:19 +0200

    Add files via upload

---
+++ Ahmed Fawzy/Example_2_Blinking_with_Switch.ino
@@ -0,0 +1,30 @@
+//Example_2_Blinking_with_Switch
+
+const int ledPin = 13;           
+const int buttonPin = 2;         
+int val;               
+
+void setup ( )
+
+ {
+  pinMode(ledPin, OUTPUT);  
+  pinMode(buttonPin, INPUT);    
+
+ }
+
+void loop()
+
+{
+  val = digitalRead(buttonPin);  
+
+  if (val == HIGH)
+     
+     {
+  digitalWrite(ledPin, HIGH);     
+  delay(1000);            
+  digitalWrite(ledPin, LOW);   
+  delay(1000);     
+      }                     
+
+  else {digitalWrite(ledPin, LOW); } }
+
