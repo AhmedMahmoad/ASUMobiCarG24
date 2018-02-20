7887cab23f34102644d251784c45c44356b5113f
ahmednasser19 <ahmednas197@gmail.com>
2018-02-20 10:06:23 -0800

    Add files via upload

---
+++ Arduino Examples/Ahmed Nasser/simple_led1.ino
@@ -0,0 +1,15 @@
+const int LED = 13;             
+                                                    
+void setup ( )
+{
+  pinMode(LED, OUTPUT);                                                       
+}
+
+void loop()
+{
+  digitalWrite(LED, HIGH);     
+  delay(1500); 
+                        
+  digitalWrite(LED, LOW);   
+  delay(1500);                          
+}
