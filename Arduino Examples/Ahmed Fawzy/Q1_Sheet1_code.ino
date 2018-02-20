3e96fa48c82db15ed656b80d97fc0be59eccba49
a7medfawzy97 <36432072+a7medfawzy97@users.noreply.github.com>
2018-02-20 06:44:27 +0200

    Add files via upload

---
+++ Arduino Examples/Ahmed Fawzy/Q1_Sheet1_code.ino
@@ -0,0 +1,16 @@
+///////// Q1 SHEET 1 ////////
+
+const int LED = 13;  
+
+void setup() 
+{
+pinMode(LED,OUTPUT);
+}
+
+void loop() {
+
+digitalWrite(LED,HIGH);
+delay(440);
+digitalWrite(LED,LOW);
+delay(1560);
+}
