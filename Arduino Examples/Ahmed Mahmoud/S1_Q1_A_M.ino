81da8f35d8cca2ac876c1f26452d6480a289a054
AhmedMahmoad <36341414+AhmedMahmoad@users.noreply.github.com>
2018-02-21 00:01:38 +0200

    Add files via upload

---
+++ Arduino Examples/Ahmed Mahmoud/S1_Q1_A_M.ino
@@ -0,0 +1,15 @@
+                                       
+void setup ( )
+{
+  pinMode(13, OUTPUT);                                                       
+}
+
+void loop()
+{
+  digitalWrite(13, HIGH);     
+  delay(500); 
+                        
+  digitalWrite(13, LOW);   
+  delay(1500);                          
+}
+
