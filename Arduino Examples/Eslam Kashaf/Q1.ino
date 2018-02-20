3dac6e38d2ef1e50f97ebeb424d587def283566b
eslamkashaf <36341093+eslamkashaf@users.noreply.github.com>
2018-02-20 14:56:18 +0200

    Add files via upload

---
+++ Arduino Examples/Eslam Kashaf/Q1.ino
@@ -0,0 +1,16 @@
+const int LED = 13;             
+                                                    
+void setup ( )
+{
+  pinMode(LED, OUTPUT);                                                       
+}
+
+void loop()
+{
+  digitalWrite(LED, HIGH);     
+  delay(500); 
+                        
+  digitalWrite(LED, LOW);   
+  delay(1500);                          
+}
+
