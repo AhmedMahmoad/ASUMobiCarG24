9b6b57116ee007ad30491437150c2b42148696ee
eslamahmed022 <36341049+eslamahmed022@users.noreply.github.com>
2018-04-08 22:57:22 +0200

    Add files via upload
    I couldn't find 3 way line tracker sensor in proteus so i used one

---
+++ Arduino Examples/Eslam Ahmed/infrared sensor code of protues.ino
@@ -0,0 +1,24 @@
+
+#include<LiquidCrystal.h>
+LiquidCrystal lcd(13,12,11,10,9,8);
+void setup(){
+  lcd.begin(16,2); 
+  
+
+  
+}
+void loop(){
+   int pirstate=digitalRead(1);
+   lcd.setCursor(0,0);
+   lcd.print("stop");
+   if(pirstate==LOW){
+        lcd.setCursor(0,1);
+           lcd.print("go");
+           delay(500);
+           lcd.clear();
+   }else if(   pirstate==HIGH){
+    lcd.setCursor(0,1);
+     lcd.print("stop");
+     delay(500);
+           lcd.clear();}
+}
