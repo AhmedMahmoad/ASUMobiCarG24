6617d0daf93f75eed77956d8519268998a16c93b
a7medfawzy97 <36432072+a7medfawzy97@users.noreply.github.com>
2018-04-09 01:09:57 +0200

    Add files via upload

---
+++ Arduino Examples/Ahmed Fawzy/Example_10_DC_Motor.ino
@@ -0,0 +1,26 @@
+//Example_10_DC_Motor
+int motorPin = 9; 
+int onTime = 2500; 
+int offTime = 1000; 
+
+void setup( ) 
+{pinMode(motorPin, OUTPUT); }
+
+void loop ( )
+{
+analogWrite(motorPin,100);
+delay(onTime);
+digitalWrite(motorPin, LOW);
+delay(offTime);
+
+analogWrite(motorPin,190);
+delay(onTime);
+digitalWrite(motorPin, LOW);
+delay(offTime);
+
+analogWrite(motorPin,255);
+delay(onTime);
+digitalWrite(motorPin, LOW);
+delay(offTime);
+}
+
