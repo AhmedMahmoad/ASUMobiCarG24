e98a68e142a1aaa6857d3f4e9a1693d3f8e2e0e2
eslamkashaf <36341093+eslamkashaf@users.noreply.github.com>
2018-04-08 20:51:09 +0200

    Add files via upload

---
+++ Arduino Examples/Eslam Kashaf/sheet_2_Q1/sheet_2_Q1.ino
@@ -0,0 +1,21 @@
+void setup() {
+  for (byte i = 0 ; i < 8 ; i++) {
+    pinMode(i , INPUT_PULLUP);
+  }
+  pinMode(8 , OUTPUT);
+  digitalWrite(8, LOW);
+  int no = 0;
+  for (byte i = 0 ; i < 8 ; i++) {
+    if (digitalRead(i) == LOW ) {
+      no += pow(2, i);
+    }
+  }
+  no *= 4;
+  digitalWrite(8, HIGH);
+  delay(no);
+  digitalWrite(8, LOW);
+}
+
+void loop() {
+  
+}
