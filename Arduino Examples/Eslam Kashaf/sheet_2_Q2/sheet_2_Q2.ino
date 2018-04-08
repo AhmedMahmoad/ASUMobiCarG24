ed09554d4e40c8ce4f628a5e14bb3d00957521c8
eslamkashaf <36341093+eslamkashaf@users.noreply.github.com>
2018-04-08 20:54:40 +0200

    Add files via upload

---
+++ Arduino Examples/Eslam Kashaf/sheet_2_Q2/sheet_2_Q2.ino
@@ -0,0 +1,29 @@
+int t = 1 ;
+byte dig = 1 ;
+byte button1 = 2 ;
+byte button2 = 3 ;
+void setup() {
+  pinMode(button1,INPUT_PULLUP) ;
+  pinMode(button2,INPUT_PULLUP) ;
+  pinMode(dig,OUTPUT) ;
+}
+
+void loop() {
+   digitalWrite(dig,HIGH) ;
+   delay(t*1000) ;
+   digitalWrite(dig,LOW) ;
+   delay(t*1000) ;
+  if((digitalRead(button1))==LOW){   
+      t = t / 2 ;
+     if(t<1){
+      t = 1 ;  
+     }
+  }
+  if((digitalRead(button2))==LOW){  
+      t = t * 2 ;
+     if(t>10){
+      t = 10 ;
+     }
+  }
+
+}
