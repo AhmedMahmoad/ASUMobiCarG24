c28bb20a27085e6831e9774b65f3f8bae99c9781
eslamkashaf <36341093+eslamkashaf@users.noreply.github.com>
2018-04-09 16:31:25 +0200

    Add files via upload

---
+++ Arduino Examples/Eslam Kashaf/Arduino_line_tracker_and_obstacle_avoiding/Arduino_line_tracker_and_obstacle_avoiding.ino
@@ -0,0 +1,40 @@
+
+const int sag_i = 2;
+const int sag_g = 3;
+const int sol_i = 5;
+const int sol_g = 4;
+const int sol_sensor = 6;
+const int sag_sensor = 7;
+const int trigger_pin = 8;
+const int echo_pin = 9;
+
+float sure;
+float mesafe;
+
+int sol_durum, sag_durum;
+int rast;
+
+void setup()
+ {
+  pinMode(sag_i, OUTPUT);
+  pinMode(sag_g, OUTPUT);
+  pinMode(sol_i, OUTPUT);
+  pinMode(sol_g, OUTPUT);
+  pinMode(sag_sensor, INPUT);
+  pinMode(sol_sensor, INPUT);
+  pinMode(trigger_pin , OUTPUT);
+  pinMode(echo_pin , INPUT);
+//  Serial.begin(9600);
+ }
+
+void loop() 
+{
+  sol_durum = digitalRead(sol_sensor);
+  sag_durum = digitalRead(sag_sensor);
+
+  digitalWrite(trigger_pin , HIGH);
+  delayMicroseconds(1000);
+  digitalWrite(trigger_pin , LOW);
+  sure = pulseIn(echo_pin , HIGH);
+  mesafe = (sure/2) / 28.5;
+}
