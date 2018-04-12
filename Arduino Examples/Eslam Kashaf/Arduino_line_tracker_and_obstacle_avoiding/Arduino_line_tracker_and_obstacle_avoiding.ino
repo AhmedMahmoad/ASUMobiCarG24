bd3872695d7a8c50e706fa7c9d4e7c5b3e26e94f
eslamkashaf <36341093+eslamkashaf@users.noreply.github.com>
2018-04-12 22:50:32 +0200

    Update Arduino_line_tracker_and_obstacle_avoiding.ino

--- Arduino Examples/Eslam Kashaf/Arduino_line_tracker_and_obstacle_avoiding/Arduino_line_tracker_and_obstacle_avoiding.ino
+++ Arduino Examples/Eslam Kashaf/Arduino_line_tracker_and_obstacle_avoiding/Arduino_line_tracker_and_obstacle_avoiding.ino
@@ -37,4 +37,136 @@ void loop()
   digitalWrite(trigger_pin , LOW);
   sure = pulseIn(echo_pin , HIGH);
   mesafe = (sure/2) / 28.5;
+//  Serial.print("sag sensor = ");
+//  Serial.println(sag_durum);
+//  Serial.print("sol sensor = ");
+//  Serial.println(sol_durum);
+//  Serial.print(" cisme olan uzaklik = ");
+//  Serial.print(mesafe);
+//  Serial.println(" cm ");
+//  Serial.println("____________________________");
+//  delay(1000);
+
+  rast = random(2);
+//  Serial.println(rast);
+
+  if (mesafe < 10) // geri , donus
+  {
+    digitalWrite(sag_g, HIGH);
+    digitalWrite(sag_i, LOW);
+    digitalWrite(sol_g, HIGH);
+    digitalWrite(sol_i, LOW);
+//    Serial.println("geri");
+    delay(190);
+    if (rast == 0) // sag
+    {
+      digitalWrite(sag_i, LOW);
+      digitalWrite(sag_g, HIGH);
+      digitalWrite(sol_i, HIGH);
+      digitalWrite(sol_g, LOW);
+//      Serial.print("sag");
+      delay(300);
+      digitalWrite(trigger_pin , HIGH);
+      delayMicroseconds(1000);
+      digitalWrite(trigger_pin , LOW);
+      sure = pulseIn(echo_pin , HIGH);
+      mesafe = (sure/2) / 28.5;
+      if (mesafe < 10) // sol
+      {
+        digitalWrite(sag_i, HIGH);
+        digitalWrite(sag_g, LOW);
+        digitalWrite(sol_i, LOW);
+        digitalWrite(sol_g, HIGH);
+//        Serial.print("sol");
+        delay(300);
+        digitalWrite(trigger_pin , HIGH);
+        delayMicroseconds(1000);
+        digitalWrite(trigger_pin , LOW);
+        sure = pulseIn(echo_pin , HIGH);
+        mesafe = (sure/2) / 28.5;
+        if (mesafe < 10)// geri
+        {
+          digitalWrite(sag_g, HIGH);
+          digitalWrite(sag_i, LOW);
+          digitalWrite(sol_g, HIGH);
+          digitalWrite(sol_i, LOW);
+//          Serial.println("geri");
+          delay(190);
+        }
+      }
+       
+    }
+    else if (rast == 1) // sol
+    {
+      digitalWrite(sag_i, HIGH);
+      digitalWrite(sag_g, LOW);
+      digitalWrite(sol_i, LOW);
+      digitalWrite(sol_g, HIGH);
+//      Serial.print("sol");
+      delay(300);
+      digitalWrite(trigger_pin , HIGH);
+      delayMicroseconds(1000);
+      digitalWrite(trigger_pin , LOW);
+      sure = pulseIn(echo_pin , HIGH);
+      mesafe = (sure/2) / 28.5;
+      if (mesafe < 10) // sag
+      {
+        digitalWrite(sag_i, LOW);
+        digitalWrite(sag_g, HIGH);
+        digitalWrite(sol_i, HIGH);
+        digitalWrite(sol_g, LOW);
+//        Serial.print("sag");
+        delay(300);
+        digitalWrite(trigger_pin , HIGH);
+        delayMicroseconds(1000);
+        digitalWrite(trigger_pin , LOW);
+        sure = pulseIn(echo_pin , HIGH);
+        mesafe = (sure/2) / 28.5;
+        if (mesafe < 10);
+        {
+          digitalWrite(sag_g, HIGH);
+          digitalWrite(sag_i, LOW);
+          digitalWrite(sol_g, HIGH);
+          digitalWrite(sol_i, LOW);
+//          Serial.println("geri");
+          delay(190); 
+        }
+      }
+    }
+  }
+      
+  else if (sol_durum == LOW && sag_durum == LOW) // ileri
+  {
+    digitalWrite(sag_i, HIGH);
+    digitalWrite(sag_g, LOW);
+    digitalWrite(sol_i, HIGH);
+    digitalWrite(sol_g, LOW);
+  }
+  else if (sol_durum == LOW && sag_durum == HIGH) // saga
+  {
+    digitalWrite(sag_i, LOW);
+    digitalWrite(sag_g, HIGH);
+    digitalWrite(sol_i, HIGH);
+    digitalWrite(sol_g, LOW);
+  }
+  else if (sol_durum == HIGH && sag_durum == LOW) // sola
+  {
+    digitalWrite(sag_i, HIGH);
+    digitalWrite(sag_g, LOW);
+    digitalWrite(sol_i, LOW);
+    digitalWrite(sol_g, HIGH);
+  }
+  else
+  {
+    digitalWrite(sag_i, LOW);
+    digitalWrite(sag_g, LOW);
+    digitalWrite(sol_i, LOW);
+    digitalWrite(sol_g, LOW);
+  }
+  delay(65);
+  digitalWrite(sag_i, LOW);
+  digitalWrite(sag_g, LOW);
+  digitalWrite(sol_i, LOW);
+  digitalWrite(sol_g, LOW);
+  delay(25); 
 }
