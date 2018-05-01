27aaa00abb49912b88f7e2e0d0405df3cabc1966
eslamkashaf <36341093+eslamkashaf@users.noreply.github.com>
2018-05-01 15:39:02 +0200

    Add files via upload

---
+++ Arduino Examples/Eslam Kashaf/sheet_2_Q4/sheet_2_Q4.ino
@@ -0,0 +1,21 @@
+void setup() {
+  pinMode(2, INPUT_PULLUP);
+  pinMode(0, OUTPUT);
+  digitalWrite(0, LOW);
+}
+int Val = 0;
+byte analogPin = -1;
+void loop() {
+  if (digitalRead(2) == LOW) {
+    if (analogPin == 5) {
+      analogPin = 0 ;
+    } else {
+      analogPin += 1;
+    }
+    Val = analogRead(analogPin);
+    digitalWrite(0, HIGH);
+    delay(Val);
+    digitalWrite(0, LOW);
+    delay(500);
+  }
+}
